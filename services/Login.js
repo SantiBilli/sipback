b7e89e49e482f1a22e8e68a434085a9338148659
SantiBilli <117537403+SantiBilli@users.noreply.github.com>
2024-05-09 16:37:47 -0300

    fix mysql down

--- services/Login.js
+++ services/Login.js
@@ -5,7 +5,7 @@ export const checkUser = async (email, contra) => {
     const consulta = "SELECT userId FROM users WHERE email = ? and contra = ?"
 
     const results = await databaseExecute(consulta, [email, contra])
-    
+
     if (results.length == 0) {
         return false
     }
