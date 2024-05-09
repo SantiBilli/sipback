766e66243657105c315a13127814eeedbffa331c
SantiBilli <117537403+SantiBilli@users.noreply.github.com>
2024-05-09 12:23:56 -0300

    ping

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import "dotenv/config"
 import registerRouter from "./routes/Register.js";
 import checkEmailRouter from "./routes/CheckEmail.js"
 import fileRouter from "./routes/Files.js";
+import pingRouter from "./routes/Ping.js";
 
 const app = express()
 const PORT = process.env.PORT || 3550
@@ -17,7 +18,7 @@ app.use("/api", loginRouter)
 app.use("/api", registerRouter)
 app.use("/api", checkEmailRouter)
 app.use("/api", fileRouter)
-
+app.use("/api", pingRouter)
 
 app.listen(PORT, () => {
     console.log(`Server listening on https//localhost:${PORT}`)
