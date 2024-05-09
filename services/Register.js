d4bbba305f7190abd25f5a560b31b789d26c4577
SantiBilli <117537403+SantiBilli@users.noreply.github.com>
2024-05-09 13:57:01 -0300

    env

--- services/Register.js
+++ services/Register.js
@@ -2,7 +2,7 @@ import { databaseExecute } from "../database/database.js";
 
 export const createUser = async (userId, email, nombre, apellido, telefono, contra) => {
 
-    const registro = "INSERT INTO usuarios (userId, email, nombre, apellido, telefono, contra) VALUES (?, ?, ?, ?, ?, ?);"
+    const registro = "INSERT INTO users (userId, email, nombre, apellido, telefono, contra) VALUES (?, ?, ?, ?, ?, ?);"
 
     const results = await databaseExecute(registro, [userId, email, nombre, apellido, telefono, contra])
 
