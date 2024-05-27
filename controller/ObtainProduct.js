8b79d51dce6ff51e325ab50c6dc693c45921201a
SantiBilli <117537403+SantiBilli@users.noreply.github.com>
2024-05-27 18:05:20 -0300

    productosDetail

--- controller/ObtainProduct.js
+++ controller/ObtainProduct.js
@@ -1,16 +1,15 @@
 import { obtainProductSVC } from "../services/ObtainProduct.js"
 
 export const obtainProductCTL = async (req, res) => {
-    const postId = req.body.postId
+    
+    const bodyParams = req.body
 
-    const producto = await obtainProductSVC(postId)
-
-    console.log(postId);
-    console.log(producto);
+    const producto = await obtainProductSVC(bodyParams.id)
 
     if (producto == false) return res.status(204).send("Product Not Found")
 
     return res.json({
         producto: producto
     })
+
 }
