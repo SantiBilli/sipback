dedd87ec0339c31c844e1e7012581a91cd36f547
SantiBilli <117537403+SantiBilli@users.noreply.github.com>
2024-06-15 20:12:18 -0300

    files

--- middlewares/FileUpload.js
+++ middlewares/FileUpload.js
@@ -1,16 +1,16 @@
 import multer from "multer";
 
-// export const storage = multer.diskStorage({
-//     destination: function (req, file, cb) {
-//       cb(null, 'uploads/')
-//     },
-//     filename: function (req, file, cb) {
-//       const uniqueSuffix = Date.now() + "_" + file.originalname
-//       // req.customFile = uniqueSuffix
-//       cb(null, uniqueSuffix)
-//     }
-//   })
+export const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'uploads/')
+    },
+    filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now() + "_" + file.originalname
+      req.customFile = uniqueSuffix
+      cb(null, uniqueSuffix)
+    }
+  })
   
-const storage = multer.memoryStorage()
+// const storage = multer.memoryStorage()
 
 export const upload = multer({ storage: storage })
