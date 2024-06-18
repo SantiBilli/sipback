bb115530fbc97748d1f28f6c43b50302da3bf19f
unknown <juanaestarli@gmail.com>
2024-06-18 20:23:29 -0300

    cambio register

--- controller/Register.js
+++ controller/Register.js
@@ -6,7 +6,7 @@ export const registerUser = async (req, res) => {
 
     const userId = v4()
 
-    const user = await createUser(userId, body.email, bodyParams.nombre, bodyParams.apellido, bodyParams.telefono, bodyParams.contra) 
+    const user = await createUser(userId, bodyParams.email, bodyParams.nombre, bodyParams.apellido, bodyParams.telefono, bodyParams.contra) 
 
     if (user === false) {
         return res.status(501).send("Error en la Base de Datos.")
