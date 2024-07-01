f25677505e9cd677e7b0c8480959797e8c50104d
SantiBilli <117537403+SantiBilli@users.noreply.github.com>
2024-07-01 13:56:00 -0300

    cambiar contra

--- routes/OlvideContraseña.js
+++ routes/OlvideContraseña.js
@@ -1,10 +1,10 @@
 import { Router } from "express"
-import { OlvideContrasenaCTL } from "../controller/OlvideContraseña.js"
+import { OlvideContrasenaCTL, ObtenerDatosContraCTL } from "../controller/OlvideContraseña.js"
 
 const olvideContrasenaRouter = Router()
 
 olvideContrasenaRouter.post("/olvide-contrasena", OlvideContrasenaCTL)
 
-olvideContrasenaRouter.get("/olvide-contrasena/:id", OlvideContrasenaCTL)
+olvideContrasenaRouter.get("/olvide-contrasena/:id", ObtenerDatosContraCTL)
 
 export default olvideContrasenaRouter
