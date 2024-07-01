9efd592f6ddb60804d79fa26965dad1efc8b19c6
SantiBilli <117537403+SantiBilli@users.noreply.github.com>
2024-07-01 01:42:57 -0300

    fotoPerfil seguridad

--- controller/File.js
+++ controller/File.js
@@ -35,7 +35,7 @@ export const fileUpload = async (req, res) => {
 
     console.log(convertedDetections);
 
-    if (convertedDetections.adult >= 3 && convertedDetections.racy >= 3) {
+    if (convertedDetections.adult >= 3 || convertedDetections.racy >= 3) {
         fs.unlinkSync(req.file.path);
         return res.status(406).send("Contenido Prohibido.")
     }
