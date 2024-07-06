2faf4c18857d5eee8c9fb08abb99835faf9a8f3a
SantiBilli <117537403+SantiBilli@users.noreply.github.com>
2024-07-06 16:37:56 -0300

    env

--- controller/Register.js
+++ controller/Register.js
@@ -10,7 +10,7 @@ export const registerUser = async (req, res) => {
     const salt = await bcrypt.genSalt(10)
     const contraHasheada = await bcrypt.hash(bodyParams.contra, salt)
 
-    const user = await createUser(userId, bodyParams.email, bodyParams.nombre, bodyParams.apellido, bodyParams.telefono, contraHasheada)
+    const user = await createUser(userId, bodyParams.email, bodyParams.nombre, bodyParams.apellido, bodyParams.telefono, contraHasheada, bodyParams.anoLectivo)
 
     if (user === false) {
         return res.status(501).send("Error en la Base de Datos.")
